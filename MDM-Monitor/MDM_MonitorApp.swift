de5d23e2a990652ec5dd3b8a04d7a85a60f29011
George Babichev <gbabichev@xantrion.com>
2026-04-02 16:46:54 -0400

    Initial Commit

---
+++ MDM-Monitor/MDM_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDM_MonitorApp.swift
+//  MDM-Monitor
+//
+//  Created by George Babichev on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MDM_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
